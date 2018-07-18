25a94d988976660595aff4a43a2de5e0a39fcc21
meta-chirag-jain <41181682+meta-chirag-jain@users.noreply.github.com>
2018-07-18 16:39:34 +0530

    Update JobScheduler.java

--- JobScheduler.java
+++ JobScheduler.java
@@ -3,26 +3,65 @@ package assignment1;
 import java.util.Scanner;
 
 public class JobScheduler {
-	/*int compTime()
-	{
-		return 0;
+	
+	int[] waitTime(int[][] job, int size) {
+		int totalTime = job[0][1];
+		int[] waitArray = new int[size];
+		int timeDiff;
+		
+		for(int i=1; i<size; i++) {
+			if( totalTime - job[i][0] >= 0) {
+				waitArray[i] = totalTime - job[i][0];
+				totalTime += job[i][1];
+			}
+			else {
+				timeDiff = job[i][0] - totalTime; 
+				totalTime = totalTime + job[i][1] + timeDiff;
+			}
+		}
+		return waitArray;
 	}
-	int waitTime()
-	{
-		return 0;
+	
+	int[] completionTime(int[][] job , int size) {
+		int[] waitArray = waitTime(job, size);
+		int[] compTime = new int[size];
+		
+		for(int i=0; i<size; i++) {
+			compTime[i] = waitArray[i] + job[i][0] + job[i][1];
+		}
+		return compTime;
 	}
-	int turnTime()
-	{
-		return 0;
+	
+	int[] turnTime(int[][] job , int size) {
+		int[] waitArray = waitTime(job, size);
+		int[] turnTime = new int[size];
+		
+		for(int i=0; i<size; i++) {
+			turnTime[i] = waitArray[i] + job[i][1];
+		}
+		return turnTime;
 	}
-	int avgWaitTime()
-	{
-		return 0;
+	
+	float avgWaitTime(int[][] job , int size) {
+		float avgWait = 0;
+		int[] waitArray = waitTime(job, size);
+		for(int i=0; i<size; i++) {
+			avgWait = avgWait + waitArray[i];
+		}
+		avgWait /= size;
+		return avgWait;
 	}
-	int maxWaitTime()
-	{
-		return 0;
-	}*/
+	
+	int maxWaitTime(int[][] job , int size) {
+		int maxWait = 0;
+		int[] waitArray = waitTime(job, size);
+		for(int i=0; i<size; i++) {
+			if(maxWait < waitArray[i]) {
+				maxWait = waitArray[i];
+			}
+		}
+		return maxWait;
+	}
 	
 
 	public static void main(String[] args) {
@@ -40,40 +79,25 @@ public class JobScheduler {
 			}
 		}
 	
+		JobScheduler jobCalc = new JobScheduler();
 		
-		int totalTime = job[0][1];
-		int[] waitArray = new int[size];
-		int timeDiff;											//initially all 0
+		int[] waitArray = jobCalc.waitTime(job, size);
 		
-		for(int i=1; i<size; i++) {
-			if( totalTime - job[i][0] >= 0)						//else wait time 0
-			{
-				waitArray[i] = totalTime - job[i][0];
-				totalTime += job[i][1];
-			}
-			else
-			{
-				timeDiff = job[i][0] - totalTime; 
-				totalTime = totalTime + job[i][1] + timeDiff;
-			}
-		}
+		int[] compArray = jobCalc.completionTime(job, size);
 		
+		int[] turnArray = jobCalc.turnTime(job, size);
 		
-		int[] compTime = new int[size];
-		int[] turnTime = new int[size];
+		float avgWaitTime = jobCalc.avgWaitTime(job, size);
 		
-		for(int i=0; i<size; i++) {
-			compTime[i] = waitArray[i] + job[i][0] + job[i][1];				//job[i][0] is arrival time
-			turnTime[i] = waitArray[i] + job[i][1];							//job[i][1] is exec time
-		}
+		int maxWaitTime = jobCalc.maxWaitTime(job, size);
 		
-		System.out.println("Array is: ");
+		System.out.println("avg wait " + avgWaitTime);
+		System.out.println("max wait " + maxWaitTime);
+
 		System.out.println("wait comp turn");
 		
 		for(int i=0; i<size; i++){
-				System.out.println(waitArray[i] + " " + compTime[i] +" " + turnTime[i]);
-		}
-		 
+				System.out.println( waitArray[i] + "\t" + compArray[i]  +"\t" + turnArray[i]);
+		}		 
 	}
-
 }
