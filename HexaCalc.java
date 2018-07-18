f3d0088030b9701e86ab9012a9314d8ca7afc3ee
meta-chirag-jain <41181682+meta-chirag-jain@users.noreply.github.com>
2018-07-18 17:43:56 +0530

    Update HexaCalc.java

--- HexaCalc.java
+++ HexaCalc.java
@@ -1,6 +1,7 @@
 /**
  * 
  * @author Chirag Jain
+ * Copyright (c) 2018 Metacube.com. All rights reserved.
  *
  */
 package assignment17july;
@@ -9,13 +10,12 @@ public class HexaCalc {
 
 	public static void main(String[] args) {
 
-		/*
-		 * int decVal = new HexToDec().convertHexToDec("F") ;
-		 * System.out.println(decVal);
-		 * 
-		 * String hexVal = new DecToHex().convertDecToHex(289);
-		 * System.out.println(hexVal);
-		 */
+		int decVal = new HexToDec().convertHexToDec("121") ;
+		System.out.println(decVal);
+		 
+		String hexVal = new DecToHex().convertDecToHex(289);
+		System.out.println(hexVal);
+		
 		String add1 = new HexCompute("A", "5").add();
 		System.out.println(add1);
 		String sub1 = new HexCompute("A", "5").sub();
@@ -24,7 +24,11 @@ public class HexaCalc {
 		System.out.println(mul1);
 		String div1 = new HexCompute("A", "5").div();
 		System.out.println(div1);
-
+		
+		HexCompare compare = new HexCompare("55", "0055");
+		System.out.println(compare.isSmaller());
+		System.out.println(compare.isGreater());
+		System.out.println(compare.isEqual());
 	}
 
 }
