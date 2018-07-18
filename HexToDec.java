c62d38c40c480204079a2592792ac48049495ea1
meta-chirag-jain <41181682+meta-chirag-jain@users.noreply.github.com>
2018-07-18 15:36:58 +0530

    Update HexToDec.java

--- HexToDec.java
+++ HexToDec.java
@@ -1,19 +1,13 @@
 package assignment17july;
+/**
+* This method converts Hexadecimal to Decimal
+* 
+* @param hex is hexadecimal value
+* @return decimal number
+*/
 
 public class HexToDec {
-
-	// private String hex;
-
-	/*
-	 * public HexToDec(String hex) { this.hex = hex; }
-	 */
-	/**
-	 * This method converts Hexadecimal to Decimal
-	 * 
-	 * @param hex
-	 *            is hexadecimal value
-	 * @return decimal number
-	 */
+	
 	public int convertHexToDec(String hex) {
 		int dec = 0;
 		String digits = "0123456789ABCDEF";
@@ -23,8 +17,7 @@ public class HexToDec {
 		for (int i = 0; i < hex.length(); i++) {
 			val = digits.indexOf(hex.charAt(i));
 			dec = (16 * dec) + val;
-		}
-
+			}
 		return dec;
+		}
 	}
-}
