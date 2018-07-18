ab2a0786c0d533b2e0b237730c55ebbf87c3b826
meta-chirag-jain <41181682+meta-chirag-jain@users.noreply.github.com>
2018-07-18 15:32:41 +0530

    Update DecToHex.java

--- DecToHex.java
+++ DecToHex.java
@@ -1,30 +1,22 @@
 package assignment17july;
-
+/***
+* This method converts Decimal to Hexadecimal
+* 
+* @param dec is decimal value
+*            
+* @return Hexadecimal number
+*/
 public class DecToHex {
-	/*
-	 * private int dec;
-	 * 
-	 * public DecToHex(int dec) { this.dec = dec; }
-	 */
-	/***
-	 * This method converts Decimal to Hexadecimal
-	 * 
-	 * @param dec
-	 *            is decimal value
-	 *            
-	 * @return Hexadecimal number
-	 */
+		
 	public String convertDecToHex(int dec) {
-		String hex = "";
-		String digits = "0123456789ABCDEF";
-		int rem;
-
-		while (dec > 0) {
-			rem = dec % 16;
-			hex = digits.charAt(rem) + hex;
-			dec = dec / 16;
-
+	String hex = "";
+	String digits = "0123456789ABCDEF";
+	int rem;
+	while (dec > 0) {
+		rem = dec % 16;
+		hex = digits.charAt(rem) + hex;
+		dec = dec / 16;
 		}
-		return hex;
+	return hex;
 	}
 }
