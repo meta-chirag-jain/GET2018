b7e24a39a02f66b871a4d6e79c5db890e6a8ff92
Chirag Jain <chirag.jain@metacube.com>
2018-08-13 09:22:35 +0530

    added test cases

--- com/metacube/DSA/Assignment2/Part2/multivariatePolynomial.java
+++ com/metacube/DSA/Assignment2/Part2/multivariatePolynomial.java
@@ -2,6 +2,12 @@ package GET2018.com.metacube.DSA.Assignment2.Part2;
 
 import java.util.List;
 
+/**
+ * Copyright (c) 2018 Metacube.com. All rights reserved.
+ * This class is used to visualize a multivariate polynomial using nested linked list.
+ * @author Chirag Jain
+ * 
+ */
 public class MultivariatePolynomial {
 
 	private List<Term> multivariate;
@@ -10,6 +16,12 @@ public class MultivariatePolynomial {
 		this.multivariate = multivariate;
 	}
 	
+	/**
+	 * Copyright (c) 2018 Metacube.com. All rights reserved.
+	 * This class defines a term of a multivariate polynomial.
+	 * @author Chirag Jain
+	 * 
+	 */
 	public static class Term {
 		public double coefficient;
 		public List<VariableList> variableList;
@@ -21,6 +33,12 @@ public class MultivariatePolynomial {
 		
 	}
 	
+	/**
+     * Copyright (c) 2018 Metacube.com. All rights reserved.
+     * This class defines all present variables in a term of multivariate polynomial.
+     * @author Chirag Jain
+     * 
+     */
 	public static class VariableList {
 		private char variable;
 		private int power;
@@ -44,6 +62,10 @@ public class MultivariatePolynomial {
 		}
 	}*/
 	
+	/**
+	 * 
+	 * @return multivariate polynomial with all the terms in it.
+	 */
 	public String visualisedMultivariatePolynomial() {
 		String polynomial = "";
 		for(Term term: multivariate) {
