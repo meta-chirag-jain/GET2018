1574ea8114d805ec8704e27f315d58a41374d4ed
Chirag Jain <chirag.jain@metacube.com>
2018-08-22 11:25:24 +0530

    added comments for DBMS Assignment 5

--- com/metacube/DBMS/Assignment5/Image.java
+++ com/metacube/DBMS/Assignment5/Image.java
@@ -1,27 +1,57 @@
 package GET2018.com.metacube.DBMS.Assignment5;
 
+/**
+ * Copyright (c) 2018 Metacube.com. All rights reserved.
+ * Class to insert images in batch.
+ * @author Chirag Jain
+ */
 public class Image
 {
     private String url;
     private String alternateText;
     
+    
+    /**
+     * constructor
+     * @param url
+     * @param alternateText
+     */
     public Image(String url, String alternateText) {
         this.url = url;
         this.alternateText = alternateText;
     }
-
+     
+    /**
+     * getter method for url
+     * @return url
+     */
     public String getUrl() {
         return url;
     }
     
+    
+    /**
+     * setter method for url
+     * @param url
+     */
     public void setUrl(String url) {
         this.url = url;
     }
     
+    
+    /**
+     * getter method for alternameText
+     * @return alternameText
+     */
     public String getAlternateText() {
         return alternateText;
     }
     
+    
+    /**
+     * setter method for alternameText
+     * @param alternateText
+     */
     public void setAlternateText(String alternateText) {
         this.alternateText = alternateText;
     }
--- com/metacube/DBMS/Assignment5/TopCategory.java
+++ com/metacube/DBMS/Assignment5/TopCategory.java
@@ -1,36 +1,69 @@
 package GET2018.com.metacube.DBMS.Assignment5;
 
+/**
+ * Copyright (c) 2018 Metacube.com. All rights reserved.
+ * Class to store the parent category name and count of its children categories.
+ * @author Chirag Jain
+ */
 public class TopCategory 
 {
     private String parentCategoryName;
-    private int childCategories;
+    private int noOfChildCategories;
     
-    public TopCategory(String parentCategoryName, int childCategories) {
+    
+    /**
+     * constructor
+     * @param parentCategoryName
+     * @param noOfChildCategories
+     */
+    public TopCategory(String parentCategoryName, int noOfChildCategories) {
         
         this.parentCategoryName = parentCategoryName;
-        this.childCategories = childCategories;
+        this.noOfChildCategories = noOfChildCategories;
     }
-
+     
+    /**
+     * getter method for parentCategoryName
+     * @return parentCategoryName
+     */
     public String getParentCategoryName() {
         return parentCategoryName;
     }
     
+    
+    /**
+     * setter method for parentCategoryName
+     * @param parentCategoryName
+     */
     public void setParentCategoryName(String parentCategoryName) {
         this.parentCategoryName = parentCategoryName;
     }
     
-    public int getChildCategories() {
-        return childCategories;
+    
+    /**
+     * getter method for noOfChildCategories
+     * @return noOfChildCategories
+     */
+    public int getNoOfChildCategories() {
+        return noOfChildCategories;
     }
     
-    public void setChildCategories(int childCategories) {
-        this.childCategories = childCategories;
+    
+    /**
+     * setter method for noOfChildCategories
+     * @param noOfChildCategories
+     */
+    public void setNoOfChildCategories(int noOfChildCategories) {
+        this.noOfChildCategories = noOfChildCategories;
     }
-
+     
+    /**
+     * method to show the TopCategory
+     */
     @Override
     public String toString() {
         return "TopCategory [parentCategoryName=" + parentCategoryName
-                + ", childCategories=" + childCategories + "]";
+                + ", childCategories=" + noOfChildCategories + "]";
     }
         
 }
